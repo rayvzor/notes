79e4634b0885ecacd892e050a3773ac8fe23f947
Andrus <zadrot-vzor2@mail.ru>
2022-07-17 12:36:32 +0300

    header

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php
+require("classes/Account.php");
+require("classes/User.php");
+session_start();
+if(isset($_SESSION['account'])){
+	switch($_SERVER['SCRIPT_NAME']){
+		//case "/logout.php":
+			//session_destroy();
+			//header("Refresh:0");
+			//break;
+		case "/login.php":
+	    case "/registration.php":
+			header("Location: index.php");
+			break;
+		default:
+			$account = $_SESSION['account'];
+	}
+}else{
+	if($_SERVER['SCRIPT_NAME'] != "/registration.php" AND $_SERVER['SCRIPT_NAME'] != "/login.php"){
+		header("Location: login.php");
+	}
+}
+?>
