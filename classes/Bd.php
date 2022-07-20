b06c91d877fa5ba9e566a055dac5db7516c7963a
Andrus <zadrot-vzor2@mail.ru>
2022-07-20 19:00:39 +0300

    bd connect

--- classes/Bd.php
+++ classes/Bd.php
@@ -3,10 +3,11 @@ class Bd{
 	public $host = "";
 	public $login = "";
 	public $password = "";
+	public $name = "";
 	private $dbLink;
 	
 	public function __construct(){
-		$this->dbLink = mysqli_connect($this->host, $this->login, $this->password);
+		$this->dbLink = mysqli_connect($this->host, $this->login, $this->password, $this->password);
 	}
 	public function bdQuery(String $sql){
 		$result = mysqli_query($this->dbLink, $sql);
--- login.php
+++ login.php
@@ -3,7 +3,7 @@ require("header.php");
 if(!empty($_POST['login']) AND !empty($_POST['password'])){
 	$login = $_POST['login'];
 	$password = $_POST['password'];
-	$result = $bd->bdQuery("SELECT id, name FROM Account WHERE login = $login AND password = $password");
+	$result = $bd->bdQuery("SELECT id, name FROM Account WHERE login = '$login' AND password = '$password'");
 	$row = mysqli_fetch_array($result);
 	if(isset($row['id'])){
 		$_SESSION['account'] = new User($row['id'], $row['name']);
